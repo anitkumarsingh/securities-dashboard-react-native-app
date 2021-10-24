074367c4b9ac975481189a58b44ac7cae338c62f
anitkumarsingh <anikumarsingh99@gmail.com>
2021-10-24 22:10:29 +0530

    final fix

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -69,8 +69,7 @@ const Home = ({ navigation }) => {
 					placeholder='Search Stocks by Security Id...'
 					onChangeText={(text) => searchFilter(text)}
 					value={searchTerm}
-					keyboardShouldPersistTaps='always'
-					keyboardDismissMode='on-drag'
+					autoFocus={true}
 				/>
 			</View>
 		);
